a964f195ebbfe59694cfd99dea753ce5f0129591
lauratakacs <lauratakacs77@gmail.com>
2024-06-20 20:12:28 +0200

    3. step

---
+++ templates/pages/fooldal.tpl.php
@@ -0,0 +1,13 @@
+
+<div id="fajtavideo">
+<h2 id="fooldalFajta" class="blockquote text-center">Az oldalon csak és kizárólag Rottweiler kutyafajta mentésével foglalkozunk. Amennyiben szeretnéd, hogy egy nagy mackó bújjon hozzád a kanapén, úgy válaszd ezt a hűséges fajtát!</h2>
+
+<h3 id="fooldalFajta" class="blockquote text-center">Az alábbi videóban megismerkedhetsz a fajtával, tulajdonságaival és természetével. A Rottweiler kutyafajta felkészülést igényel.</h3>
+
+<div class style="display: flex; justify-content: center; align-items: center; height: 60vh; margin: 0;">
+<div class="video-wrapper" style="width: 560px; height: 315px;">
+        <iframe src="https://www.youtube.com/embed/WK3zlnMvpJc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width: 100%; height: 100%;"></iframe>
+    </div>
+  </div>
+
+</div>
