c619261145ec424ec9e833ff94e999df849d86be
lauratakacs <lauratakacs77@gmail.com>
2024-06-21 18:16:08 +0200

    7. step

---
+++ logicals/footer.php
@@ -0,0 +1,5 @@
+ <div><p>Örökbeadó: <strong>Takács Laura</strong> &nbsp
+    E-mail: <a href="mailto:orokbeado@dogweiler.hu">orokbeado@dogweiler.hu</a></P></div>
+        <?php if(isset($lablec['copyright'])) { ?>&copy;&nbsp;<?= $lablec['copyright'] ?> <?php } ?>
+		&nbsp;
+        <?php if(isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
